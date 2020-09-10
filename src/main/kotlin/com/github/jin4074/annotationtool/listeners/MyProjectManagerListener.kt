14bd23ea359d916118a42370dbb311fe528cfbf1
GitHub Action <action@github.com>
2020-09-10 13:42:27 +0000

    Template cleanup

--- src/main/kotlin/com/github/jin4074/annotationtool/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/jin4074/annotationtool/listeners/MyProjectManagerListener.kt
@@ -1,8 +1,8 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.jin4074.annotationtool.listeners
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.jin4074.annotationtool.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
